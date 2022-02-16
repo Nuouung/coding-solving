955d707f9846845726ac5cb3a9f78247eff8d403
Jinseok Lee <jin6016@gmail.com>
2022-02-16 22:54:17 +0900

    sort 연습

---
+++ src/baekjoon/sort/Statics.java
@@ -0,0 +1,23 @@
+package baekjoon.sort;
+
+import java.io.*;
+
+public class Statics {
+
+    public static void main(String[] args) throws IOException {
+
+        // 효율성을 위해 Scanner 대신 BufferedReader 사용
+        // 효율성을 위해 println 대신 BufferedWriter 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        // A : N개의 수들의 합을 N으로 나눈 값
+        // B : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
+        // C : N개의 수들 중 가장 많이 나타나는 값
+        // D : N개의 수들 중 최댓값과 최솟값의 차이
+
+    }
+
+}
