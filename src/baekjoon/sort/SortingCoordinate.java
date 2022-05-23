b6ed9885ceec23823f00bf2e8ec39a23d63800a5
Nuouung <88177646+Nuouung@users.noreply.github.com>
2022-05-23 14:08:16 +0900

    Update SortingCoordinate.java

--- src/baekjoon/sort/SortingCoordinate.java
+++ src/baekjoon/sort/SortingCoordinate.java
@@ -5,37 +5,61 @@ import java.util.*;
 public class SortingCoordinate {
 
     public static void main(String[] args) {
-        // 1. x 좌표가 낮은 값 먼저 보냄
-        // 2. x 좌표가 같은 값이 있으면 y 값을 비교
-
-        // 아이디어.
-        // 1. x 값을 array로 만들어 sort.
-        // 2. x 값이 작은 순서대로 출력
-        // 3. x 값이 같으면 arrayList에 같은 값을 넣는다.
-        // 4. arrayList에 있는 값들을 y 값을 기준으로 비교
-        // 5. 출력
-        // 6. 계속 진행
 
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
 
-        int[][] arrayXAndY = new int[N][2];
-
+        int[][] beforeArray = new int[N][2];
         for (int i = 0; i < N; i++) {
-            arrayXAndY[i][0] = in.nextInt();
-            arrayXAndY[i][1] = in.nextInt();
+            beforeArray[i][0] = in.nextInt();
+            beforeArray[i][1] = in.nextInt();
         }
 
-        Arrays.sort(arrayXAndY, (valueA, valueB) -> {
-            if (valueA[0] == valueB[0]) {
-                return valueA[1] - valueB[1];
-            } else {
-                return valueA[0] - valueB[0];
-            }
-        });
+        int[][] tempArray = new int[N][2];
 
         for (int i = 0; i < N; i++) {
-            System.out.println(arrayXAndY[i][0] + " " + arrayXAndY[i][1]);
+            int min = 100002;
+            int minIndex = 0;
+            for (int j = 0; j < N; j++) {
+                if (beforeArray[j][0] < min) {
+                    min = beforeArray[j][0];
+                    minIndex = j;
+                    continue;
+                }
+            }
+
+            tempArray[i][0] = beforeArray[minIndex][0];
+            tempArray[i][1] = beforeArray[minIndex][1];
+
+            beforeArray[minIndex][0] = 100001;
+            beforeArray[minIndex][1] = 100001;
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            boolean isChange = false;
+            if (tempArray[i][0] == tempArray[i + 1][0]) {
+                if (tempArray[i][1] > tempArray[i + 1][1]) {
+                    isChange = true;
+                }
+            }
+
+            if (isChange) {
+                int x = tempArray[i][0];
+                int y = tempArray[i][1];
+
+                tempArray[i][0] = tempArray[i + 1][0];
+                tempArray[i][1] = tempArray[i + 1][1];
+
+                tempArray[i + 1][0] = x;
+                tempArray[i + 1][1] = y;
+            }
+        }
+
+        for (int[] ints : tempArray) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
         }
 
     }
