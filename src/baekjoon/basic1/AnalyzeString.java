e7861e9a8e5b877a0b2970e628ca3fbded923c18
Nuouung <jin6016@gmail.com>
2022-10-08 16:18:36 +0900

    commit

---
+++ src/baekjoon/basic1/AnalyzeString.java
@@ -0,0 +1,18 @@
+package baekjoon.basic1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AnalyzeString {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String s = br.readLine();
+            if (s.equals("")) {
+                break;
+            }
+        }
+    }
+}
