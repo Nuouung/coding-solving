7dd688c7b64d225c9d04f5be20641d5a181d42a4
Nuouung <jin6016@gmail.com>
2022-10-28 19:43:28 +0900

    commit

--- src/baekjoon/basic2/SevenDwarf.java
+++ src/baekjoon/basic2/SevenDwarf.java
@@ -1,7 +1,6 @@
 package baekjoon.basic2;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class SevenDwarf {
 
@@ -15,25 +14,30 @@ public class SevenDwarf {
             total += height;
         }
 
-        int[] sevenDwarf = new int[7];
+        List<Integer> sevenDwarf = new ArrayList<>();
+        int[] blackList = new int[2];
         for (int i = 0; i < heights.length; i++) {
+            boolean breakPoint = false;
             for (int j = i + 1; j < heights.length; j++) {
-                for (int k = j + 1; k < heights.length; k++) {
-                    if (heights[i] + heights[j] + heights[k] == (total - 100)) {
-                        for (int a = 0; a < heights.length; a++) {
-                            if (a != i && a != j && a != k) {
-                                sevenDwarf[a] = heights[a];
-                            }
-                        }
-                    }
+                if (heights[i] + heights[j] == (total - 100)) {
+                    blackList[0] = i;
+                    blackList[1] = j;
+                    breakPoint = true;
+                    break;
                 }
             }
+
+            if (breakPoint) break;
         }
 
-        Arrays.sort(sevenDwarf);
+        for (int i = 0; i < heights.length; i++) {
+            if (i != blackList[0] && i != blackList[1]) sevenDwarf.add(heights[i]);
+        }
 
-        for (int i : sevenDwarf) {
-            System.out.println(i);
+        Collections.sort(sevenDwarf);
+
+        for (Integer integer : sevenDwarf) {
+            System.out.println(integer);
         }
     }
 }
